98321c9011addf63089129b437a75d7b2a61f264
mohamad22mk <ncz.mk.ghw84@gmail.com>
2022-10-28 06:49:20 +0330

    ver 2

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from enum import auto
 import django
 from django.conf import settings
 from django.db import models
@@ -9,7 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
