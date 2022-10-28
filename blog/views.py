dd1cb3e940f3d10f3c95a0108defae4bb42eb81a
mohamad22mk <ncz.mk.ghw84@gmail.com>
2022-10-28 07:15:58 +0330

    v 2.1

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def post_list(request):
-    return render(request, "blog\post_list.html", {})
+    return render(request, "blog/post_list.html", {})
     #return HttpResponse("mamad mamamd mamad")
     
